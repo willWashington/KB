822e1d884af7cb5f213df3df8446de7e69bb5dce
William Washington <william.washington@live.com>
2018-12-21 14:39:17 -0500

    New content

--- KBMain/ExampleClass.cs
+++ KBMain/ExampleClass.cs
@@ -2,6 +2,33 @@
 using System.Collections.Generic;
 using System.Text;
 
+//what is the difference between a property and a method:
+    //properties are the gate-keepers, providing access to data
+    //methods are the operations
+//what is a constructor?
+    //a method executed when an instance is created of a class
+//what is a namespace?
+    //namespaces are used to organize classes into a logical heirarchy
+    //prevent class name collisions
+//what is a static class?
+    //a class that cannot be instantiated
+    //used for common code libraries
+//what is a singleton?
+    //a class that provides a single instance of itself
+//what is the difference between static and singleton?
+    //a static class cannot be instantiated
+    //a singleton can instantiate itself and provide that instance to other code
+
+
+/*
+ * | Namespace Do:                              |
+ * | Follow <company>.<technology>.<feature>    |
+ * | Use PascalCasing                           |
+ * | -----------------                          |
+ * | Namespace Do Not:                          |
+ * | Do not use System in your namespace        |
+ * | Do not use a class name in your namespace  |
+ * */
 namespace KBMain
 {
     /// <summary>
@@ -95,9 +122,59 @@ namespace KBMain
          * */
     }
 
-    public class ExampleClass2
+    #region Static Classes
+    //uses the static keyword in the signature
+    //class can only include static members
+    //cannot instantiate static class
+    //acts like a container for properties and methods
+    //-> that provide utility features like logging or email generation
+
+    /*
+     * | Static Classes Do:
+     * Use sparingly
+     * Should only be supporting classes
+     * Use for code library components when needed
+     * --------------
+     * | Static Classes Do Not:
+     * Do not use as miscellaneous bucket - should have purpose
+     * 
+     * 
+     * */
+
+    public static class ExampleClass2
     {
-
-
+        public static string SendMessage(string subject, string message,
+                            string recipient)
+        {
+            var confirmation = "Message sent: " + subject;
+            return confirmation;
+        }
     }
+    #endregion
+
+    #region Singleton Classes
+    //a singleton is a class that allows only one instance
+    //change constructor to private from public
+    //add static property that provides one instance
+
+    public class User
+    { //example singleton class
+        private static User instance; //backing field for instance
+        //allows code in this class to create an instance
+        private User() { }
+        public static User Instance
+        //static property that creates the one instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new User();
+                }
+                //if no instance, create an instance, else don't
+                return instance;
+            }
+        }
+    }
+    #endregion
 }
---
+++ KBMain/MoreCourses.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KBMain
+{
+    class MoreCourses
+    {
+        //Design Patterns Library
+        //Design Patterns On-Ramp
+    }
+}
